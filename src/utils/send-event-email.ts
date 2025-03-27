9b71e66962386a56ea3e4b2f0ef495e78b40579b
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2025-03-27 11:58:04 -0300

    scheduling date format

--- src/utils/send-event-email.ts
+++ src/utils/send-event-email.ts
@@ -3,15 +3,20 @@ import { Theme } from 'next-auth';
 import { Resend } from 'resend';
 import { AppError } from './app-error';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
 const resend = new Resend(env.NEXT_API_KEY);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export async function sendEventEmail(identifier: string, schedulingDate: string) {
   try {
     const schedulingDateBefore = dayjs(schedulingDate).subtract(30, 'minutes').format()
     const emailResponseBefore = await resend.emails.send({
       to: identifier,
-      from: `PetShop <noreply-dental-clinic+@${env.NEXT_EMAIL_FROM}>`,
-      subject: `Lembrete da consulta - PetShop`,
+      from: `Petshop <noreplypetshop@${env.NEXT_EMAIL_FROM}>`,
+      subject: `Lembrete do tratamento - Petshop`,
       text: text(),
       scheduledAt: schedulingDateBefore,
       html: htmlBefore(schedulingDate),
@@ -19,16 +24,13 @@ export async function sendEventEmail(identifier: string, schedulingDate: string)
 
     const emailResponseInTime = await resend.emails.send({
       to: identifier,
-      from: `PetShop <noreply-dental-clinic+@${env.NEXT_EMAIL_FROM}>`,
-      subject: `Lembrete da consulta - PetShop`,
+      from: `Petshop <noreplypetshop@${env.NEXT_EMAIL_FROM}>`,
+      subject: `Lembrete do tratamento - Petshop`,
       text: text(),
       scheduledAt: schedulingDate,
       html: htmlInTime(schedulingDate),
     })
 
-    console.log(emailResponseBefore)
-
-    console.log(emailResponseInTime)
     if (emailResponseBefore.error) {
       throw new AppError(`Erro ao enviar email: ${emailResponseBefore.error.message}`, 400);
     }
@@ -44,6 +46,10 @@ export async function sendEventEmail(identifier: string, schedulingDate: string)
 }
 
 function htmlBefore(schedulingDate: string) {
+  const schedulingDateFormat = dayjs.utc(schedulingDate)
+  .tz(dayjs.tz.guess())
+  .startOf('hour')
+  .format('DD-MM-YYYY HH:mm');
   const brandColor = "#346df1"
   const color = {
     background: "#f9f9f9",
@@ -62,15 +68,15 @@ style="background: ${color.mainBackground}; max-width: 48rem; margin: auto; bord
 <tr align="center"
 style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
 <td>
-<img src="https://dental-clinic+-two-gamma.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.9c598bf7.png&w=128&q=75" alt="Logo de Dente da PetShop"/>
-<h2>PetShop</h2>
+<img src="https://petshop-jade-xi.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.248d1717.png&w=64&q=75" alt="Logo de Petshop"/>
+<h2>Dental Clinic+</h2>
 </td>
 </tr>
 
 <tr>
 <td align="center"
 style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-"Olá! Este é um lembrete de que sua consulta está agendada para ${schedulingDate}. Ainda há tempo para se preparar. Caso não possa comparecer, entre em contato para reagendar."</td>
+"Olá! Este é um lembrete de que o tratamento do seu pet está agendado para ${schedulingDateFormat}. Ainda há tempo para se preparar. Caso não possa comparecer, entre em contato para reagendar."</td>
 </tr>
 
 <tr>
@@ -81,13 +87,18 @@ Se você não solicitou esse email, por favor apenas ignore
 </tr>
 </table>
 <footer>
-<p style="font-size: 14px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">Copyright © ${new Date().getFullYear()} PetShop </p>
+<p style="font-size: 14px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">Copyright © ${new Date().getFullYear()} Petshop </p>
 </footer>
 </body>
 `
 }
 
+
 function htmlInTime(schedulingDate: string) {
+  const schedulingDateFormat = dayjs.utc(schedulingDate)
+  .tz(dayjs.tz.guess())
+  .startOf('hour')
+  .format('DD-MM-YYYY HH:mm');
   const brandColor = "#346df1"
   const color = {
     background: "#f9f9f9",
@@ -106,15 +117,15 @@ style="background: ${color.mainBackground}; max-width: 48rem; margin: auto; bord
 <tr align="center"
 style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
 <td>
-<img src="https://dental-clinic+-two-gamma.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.9c598bf7.png&w=128&q=75" alt="Logo de Dente da PetShop"/>
-<h2>PetShop</h2>
+<img src="https://petshop-jade-xi.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.248d1717.png&w=64&q=75" alt="Logo de Dente da Dental Clinic+"/>
+<h2>Dental Clinic+</h2>
 </td>
 </tr>
 
 <tr>
 <td align="center"
 style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-Olá! Sua consulta está marcada para agora (${schedulingDate}). Estamos esperando por você! Caso tenha algum imprevisto, entre em contato com a clínica</td>
+Olá! o tratamento do seu pet está marcado para agora (${schedulingDateFormat}). Estamos esperando por você! Caso tenha algum imprevisto, entre em contato com a clínica</td>
 </tr>
 
 <tr>
@@ -125,12 +136,12 @@ Se você não solicitou esse email, por favor apenas ignore
 </tr>
 </table>
 <footer>
-<p style="font-size: 14px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">Copyright © ${new Date().getFullYear()} PetShop </p>
+<p style="font-size: 14px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">Copyright © ${new Date().getFullYear()} Petshop </p>
 </footer>
 </body>
 `
 }
 
 function text() {
-  return `Lembrete da consulta PetShop`
+  return `Lembrete tratamento do seu pet Petshop`
 }
